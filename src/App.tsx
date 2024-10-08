0537e99b6889d76d2bed44dcc75b4975a0aad8f5
ICaoVy <icaovy2001@gmail.com>
2024-10-08 12:14:40 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { Button, Layout, theme } from "antd";
+import Logo from "./components/Logo";
+import MenuList from "./components/MenuList";
+import { useState } from "react";
+import ToggleTheme from "./components/ToggleTheme";
+import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+const App = () => {
+  const { Header, Sider } = Layout;
+  const [darkTheme, setDarkTheme] = useState<boolean>(true);
+  const [conllapsed, setConllapsed] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkTheme(!darkTheme);
+  };
+
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Sider
+        collapsed={conllapsed}
+        theme={darkTheme ? "dark" : "light"}
+        className="sidebar"
+        trigger={null}
+      >
+        <Logo />
+        <MenuList darkTheme={darkTheme} />
+        <ToggleTheme toggleTheme={toggleTheme} darTheme={darkTheme} />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            className="toggle"
+            onClick={() => setConllapsed(!conllapsed)}
+            type="text"
+            icon={conllapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          />
+        </Header>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
